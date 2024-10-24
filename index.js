5d79999beb5dd2d7ceda4f753bac0032b0d7ece8
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-10-24 16:48:28 +0300

    Last commit

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const cats = ["Milo", "Otis", "Garfield"]
+const cats = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendCat(name){
-    cats.push("Ralph")
+    cats.push(name)
 }
 function destructivelyPrependCat(name){
-    cats.unshift("Bob")
+    cats.unshift(name)
 }
 function destructivelyRemoveLastCat(){
     cats.pop()
 }
 function destructivelyRemoveFirstCat(){
-    cats.shift()
+    cats.shift();
 }
-
 function appendCat(name){
     return [...cats, name]
 }
 function prependCat(name){
-    return [name, ...cats]
+    return [name,...cats]
 }
 function removeLastCat(){
     return cats.slice(0, -1)
